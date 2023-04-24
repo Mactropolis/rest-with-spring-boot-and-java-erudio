7902077f8d57f915abca7fb212dd849095d10ca4
Daniel Aoyagi <mac_registros@pobox.com>
2023-04-24 10:47:16 -0300

    sum

---
+++ 05FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/MathController.java
@@ -0,0 +1,29 @@
+package br.com.erudio;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MathController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping(value = "/sum/{numberOne}/{numberTwo}", 
+    		method = RequestMethod.GET)
+    
+    public Double sum(
+    		@PathVariable(value = "numberOne") String numberOne,
+    		@PathVariable(value = "numberTwo") String numberTwo
+    		)
+         {
+    		Double um = Double.parseDouble(numberOne);
+    		Double dois = Double.parseDouble(numberTwo);
+    		
+            return (um + dois);
+         }
+}
